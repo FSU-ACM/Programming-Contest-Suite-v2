8c4429be0702aeaf57442d7cfb978f8cb9f8d4fc
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-03-15 19:33:09 -0400

    remove files unrelated to documentation

--- src/checkin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.checkin, name='checkin'),
-    path('result/', views.checkin_result, name='checkin_result'),
-]
--- src/contestadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContestAdminConfig(AppConfig):
-    name = 'contestadmin'
